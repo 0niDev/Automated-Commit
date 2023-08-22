fee95a9a792505692390f1de454b4aafb1c5eeb8
OniChan-Baka <famalik9876@gmail.com>
2023-08-22 17:21:54 +0500

    This is a automated commit.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
         pass
     else:
         os.mkdir(path)
-    with open(rf"{path}/commit.txt", "w") as f:
-        for i in range(10):
+    for i in range(10):
+        with open(rf"{path}/commit.txt", "w") as f:
             f.write(str(i))
             os.system("git add .")
             os.system('git commit -m " This is a automated commit."')
